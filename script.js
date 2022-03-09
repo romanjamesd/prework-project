ef9693896f779aab4aa542893369e4ad1d252beb
Roman <rdarker@gmail.com>
2022-03-09 01:37:31 -0500

    style tweaks

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const h2 = document.createElement("h2");
-h2.textContent = "content added by JavaScript file";
-document.querySelector("body").appendChild(h2);
+//const h2 = document.createElement("h2");
+//h2.textContent = "content added by JavaScript file";
+//document.querySelector("body").appendChild(h2);
