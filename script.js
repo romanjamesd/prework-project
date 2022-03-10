0498df79a4d89d349de635e7b334b40c93e22e38
Roman <rdarker@gmail.com>
2022-03-10 07:15:24 -0500

    trying and failing to implement several event listeners

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-//const h2 = document.createElement("h2");
-//h2.textContent = "content added by JavaScript file";
-//document.querySelector("body").appendChild(h2);
-
-//make background-color of images opposing color when playing
-
-//check if other song is playing and turn off if so when play pressed
-
-
+//variables for elements
 let firstSongImage = document.getElementById('song1-pic');
 let secondSongImage = document.getElementById('song2-pic');
 let thirdSongImage = document.getElementById('song3-pic');
@@ -17,7 +9,8 @@ const secondSongAudio = document.getElementById('song2-audio');
 const thirdSongAudio = document.getElementById('song3-audio');
 const fourthSongAudio = document.getElementById('song4-audio');
 
-//animate image when mouse is on audio controls
-function shrinkPadding() {
+const audioFiles = [firstSongAudio, secondSongAudio, thirdSongAudio, fourthSongAudio];
+let songImages = [firstSongImage, secondSongImage, thirdSongImage, fourthSongImage];
 
-}
+
+//animate image when mouse is on audio controls
