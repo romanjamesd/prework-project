2e99629c2f53de723d4b937af56dd63e18975171
Roman <rdarker@gmail.com>
2022-03-10 04:16:11 -0500

    JS variables for images

--- script.js
+++ script.js
@@ -5,3 +5,14 @@
 //make background-color of images opposing color when playing
 
 //check if other song is playing and turn off if so when play pressed
+
+
+let firstSongImage = document.getElementById('song1-pic');
+let secondSongImage = document.getElementById('song2-pic');
+let thirdSongImage = document.getElementById('song3-pic');
+let fourthSongImage = document.getElementById('song4-pic');
+
+//animate image when mouse is on audio controls
+function shrinkPadding() {
+
+}
