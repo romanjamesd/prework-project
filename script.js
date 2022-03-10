a88156c4d39884e63e40beb6f639bae0355aaf58
Roman <rdarker@gmail.com>
2022-03-09 23:52:22 -0500

    adjust spacing among elements

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 //const h2 = document.createElement("h2");
 //h2.textContent = "content added by JavaScript file";
 //document.querySelector("body").appendChild(h2);
+
+//make background-color of images opposing color when playing
+
+//check if other song is playing and turn off if so when play pressed
